fbd6cee9e59b0caa233aca785db033e636ffd757
SeuPiorPesadelo <lucasdec88@gmail.com>
2015-10-21 22:00:08 -0200

    Update Processo.java

--- src/model/Processo.java
+++ src/model/Processo.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author marcus.rodrigues
- */
-public class Processo {
+public class Processo implements Comparable<Processo>{
 
     private String nome;
     private int tempoExe;
@@ -50,5 +41,16 @@ public class Processo {
     @Override
     public String toString() {
         return "Nome: " + nome + ", Tempo Execucao:" + tempoExe;
+    }
+
+    @Override
+    public int compareTo(Processo o) {
+        if(this.prioridade < o.prioridade){
+            return -1;
+        }else if (this.prioridade > o.prioridade) {
+            return 1;
+        }else {
+            return 0;
+        }
     }
 }
